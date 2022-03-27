21a8c544af19e0b75f81de57e4e4864849858953
kerajxl <leski.jaroslaw@gmail.com>
2022-03-27 20:56:38 +0200

    first_package_usage

--- master.R
+++ master.R
@@ -1,1 +1,8 @@
-print('test')
+library(readxl)
+library(devtools)
+library(tidyverse)
+install_github('https://github.com/kerajxl/WBreader') 
+library(WBreader)
+data <- WBreader::wb_read_excel('Data_Extract_From_World_Development_Indicators.xlsx', extension = 'xlsx', keep_NA = FALSE)
+
+
