47abac1fb9477c9f61a6374ab23fbdb0b2a113fe
kerajxl <leski.jaroslaw@gmail.com>
2022-04-21 00:46:50 +0200

    staticPlots draft

--- WB_shiny_app/app.R
+++ WB_shiny_app/app.R
@@ -1,34 +1,46 @@
-
-library(shiny)
-library(shinyWidgets)
-library(readxl)
+#library(tidyverse)
 library(devtools)
-library(tidyverse)
-install_github('https://github.com/kerajxl/WBreader') 
-#install.packages('gapminder')
-library(WBreader)
-library(rlang)
-library(gganimate)
-library(DT)
-library(tweenr)
-library(magick)
-library(gifski)
-library(magrittr)
-library(countrycode)
-library(grid)
-devtools::install_github("ellisp/ggflags")
-library(ggflags)
-library(gapminder)
+install_github('https://github.com/ellisp/ggflags')
+install_github('https://github.com/kerajxl/WBreader')
 
 
+
+packages <- c("ggplot2", "devtools", "gifski", "tidyr", "gganimate", "countrycode", 'shiny', 'shinyWidgets',
+              'tidyverse', 'rlang', 'gganimate', 'DT', 'tweenr', 'magick', 'magrittr', 'countrycode',
+              'grid', 'ggflags', 'WBreader', 'bslib', 'thematic', 'shinydashboard')
+
+
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+invisible(lapply(packages, library, character.only = TRUE))
 setwd("C:/Users/leski/OneDrive/Pulpit/DS/APR/Advanced_R_project")
+data <- WBreader::wb_read_excel(path = 'Data_Extract_From_World_Development_Indicators.xlsx', extension = 'xlsx', keep_NA = FALSE)
 
-data <- WBreader::wb_read_excel(path = 'Data_Extract_From_World_Development_Indicators.xlsx', extension = 'xlsx', keep_NA = FALSE, view = FALSE)
-codes <- read_excel('Dictrionary_codes.xlsx')
-data$value <- as.integer(data$value)
 data$year <- as.integer(data$year)
 
+data$value <- as.numeric(data$value)
+
+summary(data)
+#codes
+data$'Country Code' <- countrycode(data$'Country Name', "country.name", "iso2c")
+data$'Country Code' <- tolower(data$'Country Code')
+
+
+#Adjusts all plots in shiny to the current theme
+thematic_shiny(
+  bg = 'auto',
+  fg = 'auto',
+  accent =  'auto',
+  font =  'auto')
+
+
 ui <- fluidPage(
+  
+  #Theme
+  theme = bs_theme(),
   
   # App title ----
   titlePanel("World Bank Project"),
@@ -38,6 +50,7 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
+      
       
       
       pickerInput(
@@ -51,7 +64,11 @@ ui <- fluidPage(
       
       br(),
       
-      pickerInput('indicator', 'Select Indicator', choices = unique(data$`Series Name`) ),
+      pickerInput('indicator', 'Select Indicator x', choices = unique(data$`Series Name`),options = list(
+        `actions-box` = TRUE) ),
+      
+      pickerInput('indicator_y', 'Select Indicator y', choices = unique(data$`Series Name`), options = list(
+        `actions-box` = TRUE) ),
       
       br(),
       # Input: Slider for the number of observations to generate ----
@@ -61,42 +78,64 @@ ui <- fluidPage(
         choices = unique(data$year),
         selected = c(2005, 2015)
       )
-    
-     
+      
+      
       
     ),
     
     # Main panel for displaying outputs ----
+    
     mainPanel(
       
       # Output: Tabset w/ plot, summary, and table ----
+      
       tabsetPanel(type = "tabs",
-                  tabPanel("Plot", 
-                           plotOutput("plot")),
                   
                   tabPanel("Summary",
                            verbatimTextOutput("summary")),
                   tabPanel("Table", 
                            DTOutput("table")),
-                  tabPanel("Bar Race", 
-                           sliderInput('yearAnime', '', min = min(data$year), max = max(data$year), value = 2001, step = 1, sep = "",width = 400,
-                                       animate = animationOptions(
-                             interval = 1000,
-                             loop = TRUE,
-                             playButton = NULL,
-                             pauseButton = NULL
-                           )),
+                  tabPanel("Static Plot", 
+                           prettyRadioButtons(
+                             inputId = "selPlot",
+                             label = "Choose a graph :", 
+                             choices = c("bar", "line", "pie"),
+                             icon = icon("fa-thin fa-chart-line"),
+                             animation = "jelly",
+                             inline = TRUE, shape = 'curve'
+                           ),
                            
-                           plotOutput("bar_race"))
+                           plotOutput('staticPlot')
+                           ),
+                  
+                  tabPanel("Bar Race", uiOutput("slider1"),
+   
+                           plotOutput("bar_race")),
+                  
+                  tabPanel("Scatter Plot",
+                           
+                           #display dynamic UI
+                           uiOutput("slider2"),
+                           
+                           plotOutput("scatter_plot"),
+                           
+                           numericInput('flag_size', label = 'Adjust the size of flags: ', 10,
+                                        1, 30, 1),
+                           actionButton('render_gif', 'Export to gif'),
+                           imageOutput("plot1")
+                  )
+                  
+                  
+                  
       )
     )
-    )
-  
-)
+  )
+) 
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  bs_themer()
   dataInput <- reactive({
     plot_df <- data %>% 
       filter(year >= input$years[1] & year <= input$years[2] & `Country Name` %in% input$country & `Series Name` == input$indicator) %>% 
@@ -106,53 +145,178 @@ server <- function(input, output) {
   
   barRaceCalc <- reactive({
     data %>% 
-      left_join(codes, by = c("Country Code" = "three_code" )) %>% 
-      filter(year == input$yearAnime & `Series Name` == input$indicator) %>% 
+      #left_join(codes, by = c("Country Code" = "three_code" )) %>% 
+      filter(year == input$inSlider1 & `Series Name` == input$indicator & `Country Name` %in% input$country ) %>% 
       group_by(year) %>% 
       arrange(year,desc(value)) %>% 
       mutate(rank = row_number()*120) %>% 
-      select(year, `Country Name`, two_code, value, rank) 
+      select(year, `Country Name`, `Country Code`, value, rank)
   })
-    
-    output$bar_race <- renderPlot({
-      
-      
-      
-      
-        ggplot(barRaceCalc()) + #, aes(x = Rank, group = Country, country = as.factor(Code)
-        geom_col(aes(x = rank, y = value), width = 100, fill = "magenta", color = "black") + # Columns
-        coord_flip(clip = "off", expand = FALSE) + # Flip
-        labs(title='{closest_state}', x = "", y = "Value") + # Labels
-        theme_minimal() + # Theme
-        #geom_text(aes(x = rank, y = -1000, label = `Country Name`), hjust = 1) + # Names
-        geom_text(aes(x = rank, y = value + 200, label = as.character(value)), hjust = 0, color = "black") + # Values  
-        geom_flag(aes(x = rank, y = -300,  country = two_code), size = 10) + # Flags
-        scale_y_continuous(labels = scales::comma) + # Format y-axis values
-        scale_x_reverse() + # Highest values on top
-        #transition_states(year, transition_length = 4, state_length = 1) + # Animate
-        ggtitle((as.character(input$yearAnime)), subtitle = input$indicator)+
-        theme(
-          plot.margin = margin(0,2,0,3,"cm"),
-          axis.text.y  = element_blank()
-          )  
-      })
-      
- 
-    
-    output$table <- renderDT(
-      DT::datatable(
-       dataInput() )
-      
-      
-     
-      
-    )
-    
-    
-    
-  }
+  
   
  
+  
+  output$slider1 <- renderUI({
+    sliderInput("inSlider1", "Run the animation", min=input$years[1], max=input$years[2], value=input$years[1], step = 1, width = 400,sep = "", 
+                animate = animationOptions(
+                  interval = 1500, 
+                  loop = TRUE,
+                  playButton = NULL,
+                  pauseButton = NULL
+                ))
+  })
+  
+ 
+  
+  
+  output$bar_race <- renderPlot({
+    
+    
+    
+    data_bar <- barRaceCalc()
+    ggplot(data_bar) +
+      geom_col(aes(x = rank, y = value), width = 70) + # Columns
+      coord_flip(clip = "off", expand = FALSE) + # Flip
+      labs( x = "", y = "Value") + # Labels
+      theme_minimal() + # Theme
+      geom_text(aes(x = rank, y = value + (max(value)- min(value))*0.005, label = as.character(value)), hjust = 0) + # Values  
+      geom_flag(aes(x = rank, y = -(max(value)- min(value))*0.03,  country = unique(data_bar$'Country Code')), size = 10) + # Flags
+      scale_y_continuous(labels = scales::comma) + # Format y-axis values
+      scale_x_reverse() + # Highest values on top
+      ggtitle((as.character(input$inSlider1)), subtitle = input$indicator)+
+      theme(
+        plot.title = element_text(size = 20),
+        plot.subtitle = element_text(size = 15),
+        text = element_text(colour = "white"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.margin = margin(0,2,0,3,"cm"),
+        axis.ticks.y  = element_blank()
+        
+        
+      )   
+  }, height = 600, width = 900)
+  
+ 
+  staticPlotCalc <- reactive({
+    data %>% 
+      #left_join(codes, by = c("Country Code" = "three_code" )) %>% 
+      filter(year >= input$years[1] & year <= input$years[2] & `Series Name` == input$indicator & `Country Name` %in% input$country ) %>% 
+      select(year, `Country Name`, `Country Code`, value)
+  })
+ 
+  output$staticPlot <- renderPlot({
+    data_static <- staticPlotCalc()
+    if(input$selPlot == 'bar'){
+      ggplot(data=data_static, aes(x=`Country Name`, y=value)) +
+      geom_bar(stat="identity", fill="steelblue")+
+      theme_minimal()
+    } else if(input$selPlot == 'line'){
+      
+    } else {
+      
+    }
+    
+    
+  })
+   
+  
+  
+  output$table <- renderDT(
+    DT::datatable(
+      dataInput() )
+    
+    
+    
+    
+  )
+  
+  
+  #wprowadzam swój plot
+  
+  scatterCalc <- reactive({
+    data %>%
+      filter(input$years[1] & year <= input$years[2], `Country Name`%in% input$country,
+             `Series Name` %in% c(input$indicator, input$indicator_y)) %>% 
+      spread(key = `Series Name`, value = value) %>% 
+      filter(year == input$inSlider) %>%
+      mutate(x = input$indicator, y = input$indicator_y)
+    #group_by(year) %>% 
+    #arrange(year,desc(year)) #%>% 
+    #select(year,`Country Name`,  `Country Code`, input$indicator, input$indicator_y) 
+  })
+  
+  
+  output$scatter_plot <- renderPlot({
+    p_data <- scatterCalc()
+    
+    ggplot(p_data) +
+      aes(x = get(input$indicator), y = get(input$indicator_y)) +
+      geom_flag(aes( country=unique(`Country Code`)),size = input$flag_size, show.legend=FALSE) + 
+      labs(x = input$indicator,
+           y = input$indicator_y) +
+      #theme_minimal() +
+      ggtitle((as.character(input$inSlider)))
+    
+    
+    
+  })
+  
+  output$slider2 <- renderUI({
+    sliderInput("inSlider", "Run the animation", min=input$years[1], max=input$years[2], value=input$years[1], step = 1, width = 400,sep = "", 
+                animate = animationOptions(
+                  interval = 1000, 
+                  loop = TRUE,
+                  playButton = NULL,
+                  pauseButton = NULL
+                ))
+  })
+  
+  
+  gif_data <- reactive({
+    data %>%
+      filter(input$years[1] & year <= input$years[2], `Country Name`%in% input$country,
+             `Series Name` %in% c(input$indicator, input$indicator_y)) %>% 
+      spread(key = `Series Name`, value = value) %>% 
+      group_by(year) %>% 
+      arrange(year,desc(year))
+    
+  })
+  
+  
+  observeEvent(input$render_gif, {
+    output$plot1 <- renderImage({
+      # A temp file to save the output.
+      # This file will be removed later by renderImage
+      outfile <- tempfile(fileext='.gif')
+      
+      # now make the animation
+      p = ggplot(gif_data(), aes(x = get(input$indicator), y = get(input$indicator_y))) +
+        geom_flag(aes( country=unique(`Country Code`)),size = input$flag_size, show.legend=FALSE) + 
+        labs(x = input$indicator,
+             y = input$indicator_y) +
+        transition_time(year) # New
+      
+      anim_save("outfile.gif", animate(p)) # New
+      
+      # Return a list containing the filename
+      list(src = "outfile.gif",
+           contentType = 'image/gif',
+           width = 400,
+           height = 300,
+           alt = "This is alternate text"
+      )}, deleteFile = TRUE)
+  })
+  
+  
+  
+  
+  
+  
+}
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
